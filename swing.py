4ae207e3b9a0f50b2a1febaa652acec6654a97b5
Ryan Truran <ryan.truran@gmail.com>
2020-10-05 05:04:10 -0500

    added multi screen functionality

--- swing.py
+++ swing.py
@@ -1,11 +1,22 @@
 import pyautogui
-width, height = pyautogui.size();
+import pygetwindow as gw
+playing = True
+testing = True
+width, height = pyautogui.size()
+print(gw.getAllTitles())
+gameWindow = gw.getWindowsWithTitle('PGA TOUR 2K21')[0]
+gameWindow.activate()
+pyautogui.moveTo(gameWindow.center.x,gameWindow.center.y,0)
 
-pyautogui.moveTo(width/2,height/2,1)
-pyautogui.mouseDown()
-pyautogui.moveRel(0,400,0.60)
-pyautogui.moveRel(0,-600,0.1)
-pyautogui.mouseUp()
-pyautogui.moveTo(1455,810,1)
+if playing:
+    gameWindow.activate()
+    pyautogui.moveTo(gameWindow.center.x,gameWindow.center.y,0)
+    pyautogui.mouseDown()
+    pyautogui.moveRel(0,400,0.36)
+    pyautogui.moveRel(0,-600,0.36)
+    pyautogui.mouseUp()
+    pyautogui.moveTo(1455,810,0)
+if testing:
+    gw.getWindowsWithTitle('pySwing – swing.py PyCharm')[0].activate()
 
 
